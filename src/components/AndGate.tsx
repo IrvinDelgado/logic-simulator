3e7edc7139cc29945d08da6b804dff07d2e8871b
Irvin Delgado-Ramirez <irvind21@gmail.com>
2023-10-20 00:09:23 -0500

    chore: clean svg

--- src/components/AndGate.tsx
+++ src/components/AndGate.tsx
@@ -3,7 +3,7 @@ import { Handle, Position } from "reactflow";
 
 function AndGate({ isConnectable }: { data: any; isConnectable: boolean }) {
   return (
-    <div className="text-updater-node">
+    <div>
       <Handle
         type="target"
         position={Position.Left}
@@ -22,13 +22,13 @@ function AndGate({ isConnectable }: { data: any; isConnectable: boolean }) {
         <path
           fill="none"
           stroke="white"
-          stroke-width="2"
+          strokeWidth="2"
           d="M70 25h25M31 15H5M32 35H5"
         />
         <path
           fill="white"
           stroke="white"
-          stroke-width="2"
+          strokeWidth="2"
           d="M30 5V45H50.47619c11.267908 0 20-9.000045 20-20s-8.732091-20-20-20H30zm2.857143 2.857143H50.47619c9.760663 0 16.666667 7.639955 16.666667 17.142857 0 9.502902-7.382195 17.142857-17.142857 17.142857H32.857143V7.857143z"
         />
       </svg>
